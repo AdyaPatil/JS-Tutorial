e267f0db6f326438df4692be26c4ea8262ad3013
AdyaPatil <161324212+AdyaPatil@users.noreply.github.com>
2024-02-27 15:21:04 +0000

    conversion and comparison

---
+++ 01Basic/03_DtConversion.js
@@ -0,0 +1,57 @@
+
+
+let score = "25"
+
+//score = "25abc" it give type =NaN
+// null = 0
+//undefined =NaN
+//
+
+console.log(typeof score);
+
+//convert string to number
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+let isLoggedIn = 1
+//1 => true
+//0 => false
+//"adinath" =>true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//convert number to string
+let someNumber =39
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//*****************Operation***************** */
+
+let value =5
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 =" adinath"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1" +2 +2);
+console.log(1 +2 +"2");
+//its not correct way 
+
+
